dd57de3a35d16e1498082eeaf6c29977ff3e90ff
funnellw <will.funnell@lbi.com>
2013-02-08 10:26:23 +0000

    Improved fixture test case

--- src/test/java/fixture/TestBookingFixture.java
+++ src/test/java/fixture/TestBookingFixture.java
@@ -12,11 +12,17 @@ public class TestBookingFixture {
     @Test
     public void createBooking() throws Exception {
         BookingFixture fixture = new BookingFixture() {{
+            LeadPassengerBuilder leadPassenger = leadPasseger().withContactNumber("1234");
+
             verifyCheckinResponse("PNR_1234").and(leadPasseger().withEmail("test@test.com"));
+            verifyCheckinResponse("PNR_5678").and(leadPassenger);
         }}.build();
 
-        VerifyCheckInResponse verifyCheckInResponse = fixture.getVerifyCheckInResponse("PNR_1234");
-        assertEquals("test@test.com", verifyCheckInResponse.getLeadPassenger().getEmail());
+        VerifyCheckInResponse verifyCheckInResponse1 = fixture.getVerifyCheckInResponse("PNR_1234");
+        VerifyCheckInResponse verifyCheckInResponse2 = fixture.getVerifyCheckInResponse("PNR_5678");
+
+        assertEquals("test@test.com", verifyCheckInResponse1.getLeadPassenger().getEmail());
+        assertEquals("1234", verifyCheckInResponse2.getLeadPassenger().getContactNumber());
     }
 
 }
